44593febf1f0f9b1261d2c1a86afbc1dd2946266
miyagirei <ra.mu.umy243@gmail.com>
2022-05-27 15:24:06 +0900

    Score追加

--- Assets/miyagi/GameManager.cs
+++ Assets/miyagi/GameManager.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public static int _score;
     void Update()
     {
         
