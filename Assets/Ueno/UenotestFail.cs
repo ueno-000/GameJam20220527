d80b58eb7e5eba296d3a2812688c1988179a17ee
uen0_ <sarasa4869@zoho.com>
2022-05-27 11:19:26 +0900

    変更

--- Assets/Ueno/UenotestFail.cs
+++ Assets/Ueno/UenotestFail.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class UenotestFail : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
