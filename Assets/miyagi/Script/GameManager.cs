70aea59879708fab40134b8c20ad3d205b186e76
miyagirei <ra.mu.umy243@gmail.com>
2022-05-28 13:29:05 +0900

    GameManager調整

--- Assets/miyagi/Script/GameManager.cs
+++ Assets/miyagi/Script/GameManager.cs
@@ -6,15 +6,7 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour,IGetValue
 {
 
-    public ScoreManager scoreMana;
     [SerializeField]public int _score = 0;
-
-    public Text _resultScore;
-    void Start()
-    {
-        scoreMana.GetComponent<ScoreManager>();
-        //_resultScore.text = "Score :" + scoreMana._score;
-    }
  public void GetPoint(int score)
     {
         _score += score;
