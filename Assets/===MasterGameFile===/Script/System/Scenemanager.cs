d3cc0993f4bc4ac95d2238da39c50d5e80030731
uen0_ <sarasa4869@zoho.com>
2022-05-30 16:02:09 +0900

    SceneManagerに補足を加えた

--- Assets/===MasterGameFile===/Script/System/Scenemanager.cs
+++ Assets/===MasterGameFile===/Script/System/Scenemanager.cs
@@ -10,25 +10,31 @@ public class Scenemanager : MonoBehaviour
 {
     [Header("イメージを貼り付ける")]
     [SerializeField] Image _fadeImage;
+
     public void StartFadeOut(string scene)//フェードアウト関数
     {
         _fadeImage.gameObject.SetActive(true);
+        //徐々に暗くする→Sceneを遷移する
         this._fadeImage.DOFade(duration: 1f, endValue: 1f).OnComplete(() => SceneManager.LoadScene(scene));
         //ImageのColorは透明に設定
     }
     public void StartFadeIn()//フェードイン関数
     {
+        //徐々に明るくする→フェードに使っていたパネルを非表示にする
         this._fadeImage.DOFade(endValue: 0f, duration: 1f).OnComplete(() => _fadeImage.gameObject.SetActive(false));
         //ImageのColorは真っ黒に設定
     }
+
     public void Fade(bool type, string scene)//呼び出す関数
     {
-        if (type)
+        //フェードイン関数と同じ処理
+        if (type)// type = true:Scene遷移した後に呼び出す処理。string sceneの部分にnullと記述する必要がある。
         {
             this._fadeImage.DOFade(endValue: 0f, duration: 1f).OnComplete(() => _fadeImage.gameObject.SetActive(false));
             //ImageのColorは真っ黒に設定
         }
-        else
+        //フェードアウト関数と同じ処理
+        else//type = false:panelをactivにした後Scene遷移するからstring sceneの記述が必要
         {
             _fadeImage.gameObject.SetActive(true);
             this._fadeImage.DOFade(duration: 1f, endValue: 1f).OnComplete(() => SceneManager.LoadScene(scene));
